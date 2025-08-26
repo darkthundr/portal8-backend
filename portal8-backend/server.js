0cbd8d26b4b794cf5f3b077d275058274a0594df
darkthundr <110087865+darkthundr@users.noreply.github.com>
2025-08-26 16:47:08 +0530

    Update server.js

--- portal8-backend/server.js
+++ portal8-backend/server.js
@@ -190,8 +190,7 @@ app.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req, r
   }
 });
 
-// 🚀 Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
